db259832506f58b0c46d662ff44838617882c95b
Clément <clem.rault@outlook.com>
2021-10-18 11:21:23 +0200

    Start from scratch

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,9 @@
 package main;
 
-import java.io.FileNotFoundException;
-
-import game.Game;
-
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		Game game = new Game();
-		game.start();
+	public static void main(String[] args) {
+
 	}
 
 }
