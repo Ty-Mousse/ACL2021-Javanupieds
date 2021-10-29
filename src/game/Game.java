3dcded7098149616789fa24c9f0119812c0b931c
Clément <clem.rault@outlook.com>
2021-10-29 15:21:29 +0200

    Structure de base v2

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,13 +1,5 @@
 package game;
 
-import java.io.IOException;
-
-import elements.Level;
-
 public class Game {
-
-	public static void main(String[] args) throws IOException {
-		Level test = new Level("src/level.txt");
-	}
 
 }
---
+++ src/game/Main.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.io.IOException;
+
+import elements.Level;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Level test = new Level("src/level.txt");
+	}
+
+}
