56ce17afcd00894f29a78268057dbb1ee8420d98
PaulBoymond <92716357+PaulBoymond@users.noreply.github.com>
2021-12-03 19:03:07 +0100

    Update Controller.java

--- src/main/java/Pacman/engine/Controller.java
+++ src/main/java/Pacman/engine/Controller.java
@@ -1,5 +1,7 @@
 package main.java.Pacman.engine;
 
+import java.awt.event.KeyEvent;
+
 public class Controller {
 	
 	public Controller() {
@@ -11,9 +13,29 @@ public class Controller {
 		return nul;
 	}
 	
-	public int[] getDirection(String input) {
+	public int[] getDirection(KeyEvent e) {
 		int[] dir = {0,0};
+		switch( e.getKeyCode() ) {
+		case KeyEvent.VK_UP:
+			dir[0]=1;
+			dir[1]=0;
+			break;
+		case KeyEvent.VK_DOWN:
+			dir[0]=-1;
+			dir[1]=0;
+			break;
+		case KeyEvent.VK_RIGHT:
+			dir[0]=0;
+			dir[1]=1;
+			break;
+		case KeyEvent.VK_LEFT:
+			dir[0]=0;
+			dir[1]=-1;
+			break;
+		}
 		return dir;
+		
 	}
+	
 
 }
