02a78df84bc4189df5b7ffe45078734c1d885f1f
Clément Rault <clem.rault@outlook.com>
2022-01-09 11:00:52 +0100

    Ajout projectile

---
+++ src/main/java/Pacman/elements/Bullet.java
@@ -0,0 +1,30 @@
+package main.java.Pacman.elements;
+
+public class Bullet extends MobileElement{
+	private int dx;
+	private int dy;
+	
+	
+	public Bullet(int x, int y, int dx, int dy) {
+		super(x, y, 2, 'p'); //le '3' est a modifer, c'etait en attendant
+		this.dx = dx;
+		this.dy = dy;
+	}
+	
+	public int getdx() {
+		return this.dx;
+	}
+	
+	public int getdy() {
+		return this.dy;
+	}
+	
+	public void setdx(int dx) {
+		this.dx = dx;
+	}
+	
+	public void setdy(int dy) {
+		this.dy = dy;
+	}
+	
+}
